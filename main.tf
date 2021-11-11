07fa7d0369fb40903780b3dc98a12910f54aa7ad
abba <si181dev2021@gmail.com>
2021-11-11 16:17:20 +0200

    ci(style): formatted even more code

--- main.tf
+++ main.tf
@@ -9,6 +9,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = string
+  client_id       = string
+  client_secret   = string
+  tenant_id       = string
 }
 
 resource "azurerm_resource_group" "rg" {
